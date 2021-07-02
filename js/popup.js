6f1160fe9d8fb89d1ef697ab98c7269f9425fb6f
Whireless <83311177+Whireless@users.noreply.github.com>
2021-07-02 13:02:21 +0300

    Оживление интерактивных элементов
    Реализовал открытие и закрытие попАпа на Javascript без анимации. Локальное хранилище для хранения имени-фамилии и email не добавлял.

---
+++ js/popup.js
@@ -0,0 +1,24 @@
+const popup = document.querySelector('.feedback-popup');
+const popupOpen = document.querySelector('.map-info .button');
+const popupClose = document.querySelector('.popup-button-cross');
+const form = popup.querySelector('form');
+let formName = popup.querySelector('[id=popup-input-name]');
+let formEmail = popup.querySelector('[id=popup-input-email]');
+
+    popupOpen.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        popup.classList.add('popup-show');
+        formName.focus();
+    });
+
+    popupClose.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        popup.classList.remove('popup-show');
+    });
+
+    form.addEventListener('submit', function (evt) {
+        if (!formName.value || !formEmail.value) {
+            evt.preventDefault();
+            alert('Пожалуйста, введите ваше имя и почту, чтобы мы связались с вами.');
+        };
+    });
